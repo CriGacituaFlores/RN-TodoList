b54840a99b17564f122446d44abe515db1991ea0
Cristian <cgacitua@acid.cl>
2019-12-19 23:52:26 -0300

    add modal

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Modal } from 'react-native';
 import Item from './item'
+import Button from './button'
 import datos from './datos';
 
 export default class App extends React.Component {
 
-  handlePress = () => {
+	state = {
+		data: datos,
+		isVisible: false,
+	}
 
+  handlePress = () => {
+		this.setState({isVisible: true})
   }
 
   render() {
+		const { data } = this.state;
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, styles.gray]}>
         <View style={styles.header}>
           <Text style={styles.title}>Recordatorios</Text>  
         </View>
-        <Button title='Agregar' onPress={this.handlePress}/>
-        <FlatList data={datos} renderItem={Item} />
+				<View style={styles.view}>
+					<Button title='Agregar' onPress={this.handlePress}/>
+				</View>
+        <FlatList data={data} renderItem={Item} />
+				<Modal visible={this.state.isVisible} animationType="slide">
+					<View style={[styles.container, styles.center]}>
+						<Text style={styles.modalTitle}>Ingrese recordatorio</Text>
+					</View>
+				</Modal>
       </View>
     );
   }
@@ -26,7 +40,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
+	},
+	center: {
+		justifyContent: "center",
+		alignItems: "center"
+	},
   header: {
 		height: 100,
 		borderBottomWidth: 1,
@@ -35,6 +53,19 @@ const styles = StyleSheet.create({
 	title: {
 		textAlign: "center",
 		marginTop: 50,
+		fontSize: 28
+	},
+	view: {
+		height: 50,
+		margin: 15,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	gray: {
+		backgroundColor: "#eee"
+	},
+	modalTitle: {
 		fontSize: 28
 	}
 });
---
+++ button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, TouchableHighlight, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	button: {
+		backgroundColor: "blue",
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		borderRadius: 4
+	},
+	text: {
+		color: '#fff',
+		fontSize: 18,
+	}
+})
+
+export default ({title, onPress}) =>
+	<TouchableHighlight style={styles.button} onPress={onPress}>
+		<Text style={styles.text}>{title}</Text>
+	</TouchableHighlight>
